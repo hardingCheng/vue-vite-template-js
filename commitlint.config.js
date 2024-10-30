77d014fc4913cada3e6779e76e11e122761bd872
hardingCheng <hardingcheng@163.com>
2024-10-30 20:58:16 +0800

    feat: 更新提交校验配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -24,65 +24,30 @@ export default {
     ]
   },
   prompt: {
-    messages: {
-      type: "选择你要提交的类型 :",
-      // scope: '选择一个提交范围（可选）:',
-      // customScope: '请输入自定义的提交范围 :',
-      subject: "填写简短精炼的变更描述 :\n",
-      // body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
-      // breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-      // footerPrefixesSelect: '选择关联issue前缀（可选）:',
-      // customFooterPrefix: '输入自定义issue前缀 :',
-      // footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
-      // generatingByAI: '正在通过 AI 生成你的提交简短描述...',
-      // generatedSelectByAI: '选择一个 AI 生成的简短描述:',
-      confirmCommit: "是否提交或修改commit ?"
-    },
-    // prettier-ignore
+    // 可选类型
     types: [
-      { value: "feat",     name: "特性:     ✨  新增功能", emoji: ":sparkles:" },
-      { value: "fix",      name: "修复:        修复缺陷", emoji: ":bug:" },
-      { value: "docs",     name: "文档:        文档变更", emoji: ":memo:" },
-      { value: "style",    name: "格式:        代码格式（不影响功能，例如空格、分号等格式修正）", emoji: ":lipstick:" },
-      { value: "refactor", name: "重构:     ♻️  代码重构（不包括 bug 修复、功能新增）", emoji: ":recycle:" },
-      { value: "perf",     name: "性能:     ⚡️  性能优化", emoji: ":zap:" },
-      { value: "test",     name: "测试:     ✅  添加疏漏测试或已有测试改动", emoji: ":white_check_mark:"},
-      { value: "build",    name: "构建:      ️  构建流程、外部依赖变更（如升级 npm 包、修改 vite 配置等）", emoji: ":package:"},
-      { value: "ci",       name: "集成:        修改 CI 配置、脚本",  emoji: ":ferris_wheel:"},
-      { value: "revert",   name: "回退:     ⏪️  回滚 commit",emoji: ":rewind:"},
-      { value: "chore",    name: "其他:        对构建过程或辅助工具和库的更改（不影响源文件、测试用例）", emoji: ":hammer:"},
+      { value: "feat", name: "feat: 新增功能" },
+      { value: "fix", name: "fix: 修复功能" },
+      { value: "docs", name: "docs: 更新文档" },
+      { value: "style", name: "style: 代码格式变更" },
+      { value: "refactor", name: "refactor： 代码重构：非新增功能非修改功能" },
+      { value: "perf", name: "perf: 性能优化" },
+      { value: "test", name: "test: 增加测试用例" },
+      { value: "chore", name: "chore: 构建过程或辅助工具的变动" },
+      { value: "revert", name: "revert: 代码回退" }
     ],
-    useEmoji: true,
-    emojiAlign: "center",
-    useAI: false,
-    aiNumber: 1,
-    themeColorCode: "",
-    scopes: [],
-    allowCustomScopes: true,
-    allowEmptyScopes: true,
-    customScopesAlign: "bottom",
-    customScopesAlias: "custom",
-    emptyScopesAlias: "empty",
-    upperCaseSubject: false,
-    markBreakingChangeMode: false,
-    allowBreakingChanges: ["feat", "fix"],
-    breaklineNumber: 100,
-    breaklineChar: "|",
-    skipQuestions: [],
-    issuePrefixes: [{ value: "closed", name: "closed:   ISSUES has been processed" }],
-    customIssuePrefixAlign: "top",
-    emptyIssuePrefixAlias: "skip",
-    customIssuePrefixAlias: "custom",
-    allowCustomIssuePrefix: true,
-    allowEmptyIssuePrefix: true,
-    confirmColorize: true,
-    maxHeaderLength: Infinity,
-    maxSubjectLength: Infinity,
-    minSubjectLength: 0,
-    scopeOverrides: undefined,
-    defaultBody: "",
-    defaultIssues: "",
-    defaultScope: "",
-    defaultSubject: ""
+    // 消息步骤
+    messages: {
+      type: "请选择提交类型:",
+      customScope: "请输入修改范围(可选):",
+      subject: "请简要描述提交(必填):",
+      body: "请输入详细描述(可选):",
+      footer: "请输入要关闭的issue(可选):",
+      confirmCommit: "确认使用以上信息提交？(y/n/e/h)"
+    },
+    // 跳过问题
+    skipQuestions: ["body", "footer"],
+    // subject文字长度默认是72
+    subjectLimit: 72
   }
 };
